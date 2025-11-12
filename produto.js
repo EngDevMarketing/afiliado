baa5f8e0f8ffa20e1fa77a201efdf54f431cb6c6
EngDevMarketing <eng.dev@outlook.com>
2025-11-12 01:56:41 -0300

    Add files via upload

---
+++ produto.js
@@ -0,0 +1,163 @@
+window.produtos = [
+    {
+        id: 2001,
+        nome: "Samsung Galaxy A26",
+        nome_personalizado: "Samsung Galaxy A26",
+        descricao: "Celular Samsung Galaxy A26 5g 256gb, 8gb Ram, Câmera De 50mp, Ip67, Tela Super Amoled 6.7 , Nfc - Preto",
+        imagem: "imagens/galaxA26.webp",
+        fabricante: "samsung",
+        link: "https://mercadolivre.com/sec/2A2K8pE",
+        especificacoes: [ 
+            "Celular Samsung Galaxy A26 5g 256gb, 8gb Ram, Câmera De 50mp, Ip67, Tela Super Amoled 6.7 , Nfc - Preto"
+        ]
+    },
+    {
+        id: 2002,
+        nome: "Smartphone Motorola Moto g05",
+        nome_personalizado: "Smartphone Motorola Moto g05",
+        descricao: "Moto g05 - 128GB 12GB (4GB RAM + 8GB Ram Boost) e Camera 50MP com AI Bateria de 5200 mAh Tela 6.7 com Superbrilho Violeta",
+        imagem: "imagens/Moto g 05.webp",
+        fabricante: "Motorola",
+        link: "https://mercadolivre.com/sec/2RgfV2e",
+        especificacoes: [ 
+            "Moto g05 - 128GB 12GB (4GB RAM + 8GB Ram Boost) e Camera 50MP com AI Bateria de 5200 mAh Tela 6.7 com Superbrilho Violeta"
+        ]
+    },
+    {
+        id: 2003,
+        nome: "Apple iPhone 15",
+        nome_personalizado: "APARADOR BUFFET FUTURE",
+        descricao: "Apple iPhone 15 (128 GB) - Preto - Distribuidor Autorizado Apple",
+        imagem: "imagens/Apple iPhone 15.webp",
+        fabricante: "Apple",
+        link: "https://mercadolivre.com/sec/19hY15w",
+        especificacoes: [ 
+            "Apple iPhone 15 (128 GB) - Preto - Distribuidor Autorizado Apple"
+        ]
+    },
+
+    {
+        id: 2004,
+        nome: "iPhone 16e (128 GB)",
+        nome_personalizado: "iPhone 16e (128 GB)",
+        descricao: "Apple iPhone 16 (128 GB) - Branco - Distribuidor Autorizado Apple",
+        imagem: "imagens/iPhone 16e (128 GB).webp",
+        fabricante: "Apple",
+        link: "https://mercadolivre.com/sec/2KC2tPF",
+        especificacoes: [ 
+            "Apple iPhone 16 (128 GB) - Branco - Distribuidor Autorizado Apple"
+        ]
+    },
+
+    {
+        id: 2005,
+        nome: "Smartphone Motorola Moto G56",
+        nome_personalizado: "Smartphone Motorola Moto G56",
+        descricao: "Moto G56 5g 256gb 16gb 8gb Ram+8gb Ram Boost Camera 50mp Sony Lytia 600 certificado de Ultrarresistencia Militar e Ip68 + Ip69 Grafite",
+        imagem: "imagens/moto g56.webp",
+        fabricante: "Motorola",
+        link: "https://mercadolivre.com/sec/2bC5iiK",
+        especificacoes: [ 
+            " Moto G56 5g 256gb 16gb 8gb Ram+8gb Ram Boost Camera 50mp Sony Lytia 600 certificado de Ultrarresistencia Militar e Ip68 + Ip69 Grafite"
+        ]
+    },
+    {
+        id: 2006,
+        nome: "Smartphone Motorola Edge 60",
+        nome_personalizado: "Smartphone Motorola Edge 60",
+        descricao: "Smartphone Motorola Edge 60 Fusion 5g - 256gb 16gb (8gb Ram+8gb Ram Boost) 50mp Sony Ai Camera Ip68 Nfc - Cinza Vegan Leather",
+        imagem: "imagens/Motorola Edge 60.webp",
+        fabricante: "Motorola",
+        link: "https://mercadolivre.com/sec/2dRZ3kp",
+        especificacoes: [ 
+            " Smartphone Motorola Edge 60 Fusion 5g - 256gb 16gb (8gb Ram+8gb Ram Boost) 50mp Sony Ai Camera Ip68 Nfc - Cinza Vegan Leather"
+        ]
+    },
+    {
+        id: 2006,
+        nome: "Smartphone Motorola Edge 60 Pro",
+        nome_personalizado: "Smartphone Motorola Edge 60 Pro",
+        descricao: "Motorola Edge 60 Pro 5g - 256gb 24gb (12gb Ram+12gb Ram Boost) Tela Quad-curve Moto Ai 50mp Sony Camera Ultrarresistencia Militar Ip68 + Ip69 - Azul",
+        imagem: "imagens/Motorola Edge 60 Pro.webp",
+        fabricante: "Motorola",
+        link: "https://mercadolivre.com/sec/2habrpT",
+        especificacoes: [ 
+            " Motorola Edge 60 Pro 5g - 256gb 24gb (12gb Ram+12gb Ram Boost) Tela Quad-curve Moto Ai 50mp Sony Camera Ultrarresistencia Militar Ip68 + Ip69 - Azul"
+        ]
+    },
+    {
+        id: 2006,
+        nome: "Samsung Galaxy S24 Ultra",
+        nome_personalizado: "Samsung Galaxy S24 Ultra",
+        descricao: "Samsung Galaxy S24 Ultra, 256gb, 12gb Ram, 6.8 , Ai Cor Titânio-cinza",
+        imagem: "imagens/Galaxy S24 Ultra.webp",
+        fabricante: "Samsung",
+        link: "https://mercadolivre.com/sec/2bMgssq",
+        especificacoes: [ 
+            " Samsung Galaxy S24 Ultra, 256gb, 12gb Ram, 6.8 , Ai Cor Titânio-cinza"
+        ]
+    },
+    {
+        id: 2006,
+        nome: "Samsung Galaxy S25+",
+        nome_personalizado: "Samsung Galaxy S25+",
+        descricao: "Celular Samsung Galaxy S25+ 5g, 512gb, 12gb Ram, Câmera Tripla De 50+12+10, Tela Grande De 6.7 Azul Marinho",
+        imagem: "imagens/Samsung Galaxy S25+.webp",
+        fabricante: "Samsung",
+        link: "https://mercadolivre.com/sec/1ebvsXU",
+        especificacoes: [ 
+            "Celular Samsung Galaxy S25+ 5g, 512gb, 12gb Ram, Câmera Tripla De 50+12+10, Tela Grande De 6.7 Azul Marinho "
+        ]
+    },
+    {
+        id: 2006,
+        nome: "APARADOR BUFFET QUARTZO",
+        nome_personalizado: "APARADOR BUFFET QUARTZO",
+        descricao: "Aparador Buffet Quartzo com 3 Portas - Moveis JB Bechara",
+        imagem: "imagens/2006_cina_off.jpg",
+        fabricante: "Moveis Bechara",
+        link: "https://www.mercadolivre.com.br/search?q=aparador+quartzo+bechara",
+        especificacoes: [ 
+            " "
+        ]
+    },
+    {
+        id: 2006,
+        nome: "APARADOR BUFFET QUARTZO",
+        nome_personalizado: "APARADOR BUFFET QUARTZO",
+        descricao: "Aparador Buffet Quartzo com 3 Portas - Moveis JB Bechara",
+        imagem: "imagens/2006_cina_off.jpg",
+        fabricante: "Moveis Bechara",
+        link: "https://www.mercadolivre.com.br/search?q=aparador+quartzo+bechara",
+        especificacoes: [ 
+            " "
+        ]
+    },
+    {
+        id: 2005,
+        nome: "Parafusadeira Furadeira",
+        nome_personalizado: "Parafusadeira Furadeira",
+        descricao: "Parafusadeira Furadeira De Impacto The Black Tools Profissional TB-21PX 2 Baterias Com Maleta 60Hz Amarelo",
+        imagem: "imagens/DkitParafusadeira.webp",
+        fabricante: "The Black Tools",
+        link: "https://mercadolivre.com/sec/1zxCW1z",
+        especificacoes: [ 
+            "Parafusadeira Furadeira De Impacto The Black Tools Profissional TB-21PX 2 Baterias Com Maleta 60Hz Amarelo"
+        ]
+    },
+    {
+        id: 2006,
+        nome: "APARADOR BUFFET QUARTZO",
+        nome_personalizado: "APARADOR BUFFET QUARTZO",
+        descricao: "Aparador Buffet Quartzo com 3 Portas - Moveis JB Bechara",
+        imagem: "imagens/2006_cina_off.jpg",
+        fabricante: "Moveis Bechara",
+        link: "https://www.mercadolivre.com.br/search?q=aparador+quartzo+bechara",
+        especificacoes: [ 
+            "Cor: CINAMOMO/ OFF WHITE, MADEIRA RÚST/ PRETO FOSCO, PRETO FOSCO",
+            "Altura: 92cm",
+            "Largura: 136cm",
+            "Profundidade: 36cm"
+        ]
+    }
+];
